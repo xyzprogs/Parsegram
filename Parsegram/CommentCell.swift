1b0b911542e14a6e0692b0bfef3078b5a9d796e1
Andy <zhengxinyao888@gmail.com>
2021-10-16 20:45:32 -0400

    comments displayed

---
+++ Parsegram/CommentCell.swift
@@ -0,0 +1,25 @@
+//
+//  CommentCell.swift
+//  CommentCell
+//
+//  Created by Andy Zheng on 10/16/21.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
